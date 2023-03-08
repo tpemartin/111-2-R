c7661f0495dff67c264adf224482fa6656753947
Martin老師 <tpemartin@gmail.com>
2023-03-08 19:14:28 +0800

    week 3

---
+++ week3.R
@@ -0,0 +1,24 @@
+read.csv("data/opendata110f041.csv")
+
+# bind a value to an object (data)
+data <- read.csv("data/opendata110f041.csv")
+
+# take of your target column
+## Q: how to retrieve the column of a data frame
+## A: data$Name
+
+x <- data$average_age
+class(x)
+# compute mean
+## Q: how to compute average in R
+
+# wrong
+# mean(data$average_age)
+# mean(x)
+x
+as.numeric(x)
+numeric_x <- as.numeric(x)
+
+# wrong
+# mean(numeric_x)
+mean(numeric_x, na.rm = TRUE)
